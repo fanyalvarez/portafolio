f40dc4f7993594af90678f8dd48ff842323f6499
fany <alvarezpestefania@outlook.com>
2023-04-13 00:29:24 -0600

    add a subtitle and projects links

--- script.js
+++ script.js
@@ -19,3 +19,34 @@ const aboutContainerEl = document.getElementById('aboutContainer')
 // 	setTimeout("location.reload(true);",timeoutPeriod);
 // }
 // window.onload = timedRefresh(6000);
+
+
+
+// funcion subtitle
+const textAboutEl = document.getElementById('textAbout')
+const textSkillsEl = document.getElementById('textSkills')
+const textProjectsEl = document.getElementById('textProjects')
+const textContactEl = document.getElementById('textContact')
+const textAbout = 'Sobre mi '
+const textSkills = 'Skills'
+const textProjects = 'Proyectos'
+const textContact = 'Contacto'
+
+let idx = 0
+
+writeText()
+
+function writeText() {
+    textAboutEl.innerText = textAbout.slice(0, idx)
+    textSkillsEl.innerText = textSkills.slice(0, idx)
+    textProjectsEl.innerText = textProjects.slice(0, idx)
+    textContactEl.innerText = textContact.slice(0, idx)
+
+    idx++
+
+    if (idx > 13) {
+        idx = 1
+    }
+    setTimeout(writeText, 150)
+}
+
